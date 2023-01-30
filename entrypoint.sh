b98051308ad59e0117a52ea4ed82ad9b746f9b99
likedidu <91383267+likedidu@users.noreply.github.com>
2023-01-30 09:18:54 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-/book-searcher index -f *.csv
-/book-searcher run -b '0.0.0.0:7070' &
+/app/book-searcher index -f *.csv &
+/app/book-searcher run -b '0.0.0.0:7070' &
 
 cp /app/nginx.conf /etc/nginx/conf.d/server.conf 
 
