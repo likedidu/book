5ab0c03c9d78ec9a1caac1a4f599ee0f8dd66b78
likedidu <91383267+likedidu@users.noreply.github.com>
2023-01-30 09:12:03 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 /book-searcher index -f *.csv
 /book-searcher run -b '0.0.0.0:7070' &
 
-cp /nginx.conf /etc/nginx/conf.d/server.conf 
+cp /app/nginx.conf /etc/nginx/conf.d/server.conf 
 
 rm -rf /etc/nginx/sites-enabled/default
 nginx -g 'daemon off;'
